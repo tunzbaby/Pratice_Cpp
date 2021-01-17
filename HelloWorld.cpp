73ef4087691187b0c9d490960fa930a433062c57
tunzbaby <phamtung@outlook.com>
2021-01-17 22:30:56 +0700

    update morefile

--- HelloWorld.cpp
+++ HelloWorld.cpp
@@ -2,30 +2,26 @@
 // using namespace std
 using namespace std;
 
+// Khai báo và định nghĩa hàm đầy đủ ở đây, trước hàm main
+float f_thi_du(float x, int b, int c)
+{
+    float gia_tri; // Khai bao bien cuc bo
+    gia_tri = x*x+b*x+c;
+    return gia_tri;
+}
+
 int main()
 {
-    // Khai bao ham prototype,
-    float f_thi_du(float, int, int); // Luu y co dau ; o cuoi
+    //float f_thi_du(float, int, int) không cần nữa
+    float x=1.5;
+    float y, z;
+    int n=3, p=5, q=10;
 
-    // Khai bao cac bien khac
-    float x = 1.5;
-    int n = 3, p = 5, q = 10;
-    float y,z;
-
-    y = f_thi_du(x,n,p); // su dung ham thu nhat
+    y = f_thi_du(x, n, p);
     cout << "Gia tri cua y = " << y << endl;
 
-    z = f_thi_du(x+0.5, q, n-1); // Su dung ham lan thu hai
+    z= f_thi_du(x+0.5, q, n-1);
     cout << "Gia tri cua z = " << z << endl;
 
     return 0;
-}
-
-/* Dinh nghia ham */
-float f_thi_du(float x, int b, int c)
-{
-    float gia_tri; /* Khai bao bien cuc bo */
-    gia_tri = x*x + b*x +c;
-
-    return gia_tri; 
 }
